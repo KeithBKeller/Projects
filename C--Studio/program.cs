a2f4db15e3da2bc3c651c21c0a4371427f87a358
Keith Keller <44787362+KeithBKeller@users.noreply.github.com>
2018-12-10 15:39:48 -0700

    Create program.cs

---
+++ C--Studio/program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+            Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
+            Console.ReadLine();
+        }
+    }
+}
